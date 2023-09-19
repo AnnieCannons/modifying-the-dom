df336ae9effc4abfc02c470b32baafdd92b86abc
Taylor <taylorlange95@gmail.com>
2023-09-19 11:09:39 -0500

    started.

--- website-1/scripts.js
+++ website-1/scripts.js
@@ -1,6 +1,8 @@
 // ********************** Exercise 1 ********************** //
 // Use JavaScript to change the text inside the <h1> element to "DOM Updates".
 
+let headingElement = document.getElementById("heading");
+headingElement.textContent = "DOM Updates";
 
 
 
@@ -9,6 +11,12 @@
 
 // ********************** Exercise 2 ********************** //
 // In the last div, use JavaScript to change the paragraph element to a span element.
+
+let changeTag = document.getElementById(`#div2 p`);
+
+let newSpan = document.createElement(`span`);
+
+// changeTag.
 
 
 
@@ -25,10 +33,14 @@
 
 // ********************** Exercise 4 ********************** //
 // Console log the value the user types into the input field.
+let formInput = getElementsByTagName('input')
 
-
-
-
+console.log(formInput.value)
 
 // ********************** Exercise 4 ********************** //
 // Change the background color of the <div> element with the ID "container" to blue.
+
+//1. add a css file 
+//2. Add a class name to the div with the ID of container
+//3. Use that class name in your css to turn the background color to any color you like
+//4. Write
