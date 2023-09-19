be31c41217ac1b6dfada28c3de3441843e2de933
shan wolff <shanwolff182@gmail.com>
2023-09-19 11:09:23 -0700

    exercises done

--- website-1/scripts.js
+++ website-1/scripts.js
@@ -1,6 +1,6 @@
 // ********************** Exercise 1 ********************** //
 // Use JavaScript to change the text inside the <h1> element to "DOM Updates".
-
+document.querySelector("h1").innerText = "DOM Updates";
 
 
 
@@ -9,7 +9,8 @@
 
 // ********************** Exercise 2 ********************** //
 // In the last div, use innerHTML to replace all of the content with a span tag that has the text 'Hello World'.
-
+let lastDiv = document.getElementById('lastDiv');
+lastDiv.innerHTML = '<span>Hello World</span>';
 
 
 
@@ -18,7 +19,10 @@
 
 // ********************** Exercise 3 ********************** //
 // Add a new list item to the <ul> element with the ID "list".
-
+let list = document.getElementById("list");
+let listItem = document.createElement("li"); 
+listItem.textContent = "Item 4"; 
+list.appendChild(listItem);
 
 
 
@@ -28,3 +32,4 @@
 
 // ********************** Exercise 4 ********************** //
 // Change the background color of the <div> element with the ID "container" to blue.
+document.getElementById("container").style.backgroundColor = "blue";
