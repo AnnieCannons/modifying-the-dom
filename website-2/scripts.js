d924a0fc875986f11f90a7d775461e6ec6f206f1
Taylor <taylorlange95@gmail.com>
2023-09-19 14:51:58 -0500

    completed website 2

--- website-2/scripts.js
+++ website-2/scripts.js
@@ -1,25 +1,37 @@
 // Complete the function
-
+// console.log("hello");
 function addItem() {
     // Get the user input
-    let newItemText = // add your code here 
+    //Taking the text the user types into the form and saving in a variable
+    let newItemText = document.getElementById("newTask").value;
+
+    //checking to make sure the user typed into the input before clicking the button; if they did not, run the alert 
+     if (newItemText === "") {
+        alert("Please enter an item")
+    }
 
     // Check if the input is empty. If it is empty, return a alert that says, "Please enter an item"
     
     //add your code here
 
     // Create a new list item element
-    var listItem = // add your code here
-    
-    // Set the text content of the new list item
+
+   let listItem = document.createElement("li");
+    //let textNode = document.createTextNode(newItemText);
+    //listItem.appendChild(textNode);
+    //document.getElementById("taskList").appendChild("li"); 
+   // Set the text content of the new list item
     listItem.textContent = newItemText;
-
+console.log(listItem.textContent);
     // Get the list where we want to add the new item
-    var itemList = // add your code here
-
+    let itemList = document.getElementById("taskList");
+console.log(itemList);
     // Append the new list item to the list
     itemList.appendChild(listItem);
 
     // Clear the input field
-    document.getElementById("newItem").value = "";
+    document.getElementById("newTask").value = "";
 }
+
+document.getElementById("addButton").addEventListener("click", addItem);
+
