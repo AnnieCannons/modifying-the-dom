cfa0a1a04149fd5ad9d3caa97ba3e641fd842163
[Hailey G] <[haileydgrace@gmail.com]>
2023-09-19 01:25:35 -0400

    Completed Assignment

--- website-1/scripts.js
+++ website-1/scripts.js
@@ -1,26 +1,25 @@
 // ********************** Exercise 1 ********************** //
 // Use JavaScript to change the text inside the <h1> element to "DOM Updates".
 
-
-
-
-
+document.getElementById("heading").textContent = "Dom Updates";
 
 
 // ********************** Exercise 2 ********************** //
 // In the last div, use JavaScript to change the paragraph element to a span element.
 
+let text = document.querySelector(".lastDiv p")
 
+let newElement = document.createElement("span")
 
-
-
+text.parentNode.replaceChild(newElement, text)
 
 
 // ********************** Exercise 3 ********************** //
 // Add a new list item to the <ul> element with the ID "list".
 
-
-
+const newItem = document.createElement("li");
+newItem.textContent = "Item 4";
+document.getElementById("list").appendChild(newItem);
 
 
 // ********************** Exercise 4 ********************** //
@@ -28,7 +27,6 @@
 
 
 
-
-
 // ********************** Exercise 4 ********************** //
 // Change the background color of the <div> element with the ID "container" to blue.
+document.getElementById("container").style.backgroundColor = 'lightblue'
--- website-2/scripts.js
+++ website-2/scripts.js
@@ -2,24 +2,28 @@
 
 function addItem() {
     // Get the user input
-    let newItemText = // add your code here 
+    let newItemText = document.getElementById("newTask").value
+
 
     // Check if the input is empty. If it is empty, return a alert that says, "Please enter an item"
     
-    //add your code here
+    if (newItemText === "") {
+        alert ("Please enter an item") }
+    
+    
 
     // Create a new list item element
-    var listItem = // add your code here
-    
+    var listItem =  document.createElement("li");
+
     // Set the text content of the new list item
-    listItem.textContent = newItemText;
+    listItem.textContent = newItemText
 
     // Get the list where we want to add the new item
-    var itemList = // add your code here
+    var itemList = document.getElementById("taskList")
 
     // Append the new list item to the list
     itemList.appendChild(listItem);
 
     // Clear the input field
-    document.getElementById("newItem").value = "";
+    document.getElementById("newTask").value = "";
 }
