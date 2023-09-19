788fa30c5a410e11e1589f48a77079c0be95b77b
Taylor <taylorlange95@gmail.com>
2023-09-19 12:30:47 -0500

    completed website 1.

--- website-1/scripts.js
+++ website-1/scripts.js
@@ -10,13 +10,18 @@ headingElement.textContent = "DOM Updates";
 
 
 // ********************** Exercise 2 ********************** //
-// In the last div, use JavaScript to change the paragraph element to a span element.
+// // In the last div, use innerHTML to replace all of the content with a span tag that has the text 'Hello World'.
 
-let changeTag = document.getElementById(`#div2 p`);
+let changeTag = document.querySelector(`#test`);
+changeTag.innerHTML = "<span>Hello World</span>";
+console.log(changeTag);
 
-let newSpan = document.createElement(`span`);
 
-// changeTag.
+//let newSpan = document.createElement(`span`);
+
+//changeTag = newSpan;
+
+
 
 
 
@@ -27,18 +32,20 @@ let newSpan = document.createElement(`span`);
 // ********************** Exercise 3 ********************** //
 // Add a new list item to the <ul> element with the ID "list".
 
+let listItem = document.querySelector(`#list`);
+let newListItem= document.createElement("li");
+let liContent = document.createTextNode("test");
+newListItem.appendChild(liContent);
+document.body.insertBefore(newListItem, listItem);
 
 
 
 
-// ********************** Exercise 4 ********************** //
-// Console log the value the user types into the input field.
-let formInput = getElementsByTagName('input')
-
-console.log(formInput.value)
 
 // ********************** Exercise 4 ********************** //
 // Change the background color of the <div> element with the ID "container" to blue.
+
+let changeColor = document.getElementById("container").style.backgroundColor = "blue";
 
 //1. add a css file 
 //2. Add a class name to the div with the ID of container
