07d81d3161b006a89c186850c5b42b542b559bef
Stay-Hack <staysha.l.hackmann@gmail.com>
2023-10-08 07:19:42 -0700

    complete

--- website-1/scripts.js
+++ website-1/scripts.js
@@ -1,7 +1,8 @@
 // ********************** Exercise 1 ********************** //
 // Use JavaScript to change the text inside the <h1> element to "DOM Updates".
 
-
+let headingElement = document.getElementById("heading");
+headingElement.textContent = "DOM Updates";
 
 
 
@@ -10,7 +11,14 @@
 // ********************** Exercise 2 ********************** //
 // In the last div, use innerHTML to replace all of the content with a span tag that has the text 'Hello World'.
 
+let changeTag = document.querySelector(`#test`);
+changeTag.innerHTML = "<span>Hello World</span>";
+console.log(changeTag);
 
+
+//let newSpan = document.createElement(`span`);
+
+//changeTag = newSpan;
 
 
 
@@ -19,7 +27,11 @@
 // ********************** Exercise 3 ********************** //
 // Add a new list item to the <ul> element with the ID "list".
 
-
+let listItem = document.querySelector(`#list`);
+let newListItem= document.createElement("li");
+let liContent = document.createTextNode("test");
+newListItem.appendChild(liContent);
+document.body.insertBefore(newListItem, listItem);
 
 
 
@@ -28,3 +40,4 @@
 
 // ********************** Exercise 4 ********************** //
 // Change the background color of the <div> element with the ID "container" to blue.
+document.getElementById("container").style.color = "blue";
