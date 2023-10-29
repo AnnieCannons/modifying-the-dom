c42d93ffc606340ce1125b5746c305d0d2625fe4
Dorcus Nagadya <dorcusnagadya889@gmail.com>
2023-10-28 17:05:22 -0700

    final

--- website-1/scripts.js
+++ website-1/scripts.js
@@ -1,34 +1,32 @@
 // ********************** Exercise 1 ********************** //
 // Use JavaScript to change the text inside the <h1> element to "DOM Updates".
 
-
-
-
-
-
+const h1Element = document.querySelector('h1');
+h1Element.textContent = "DOM Updates";
 
 // ********************** Exercise 2 ********************** //
 // In the last div, use JavaScript to change the paragraph element to a span element.
 
-
-
-
-
-
+const lastDivElement = document.querySelectorAll('div').item(document.querySelectorAll('div').length - 1);
+lastDivElement.innerHTML = '<span>Hello World</span>';
 
 // ********************** Exercise 3 ********************** //
 // Add a new list item to the <ul> element with the ID "list".
 
-
-
+const ulElement = document.getElementById('list');
+const newListItem = document.createElement('li');
+newListItem.textContent = 'New List Item';
+ulElement.appendChild(newListItem);
 
 
 // ********************** Exercise 4 ********************** //
 // Console log the value the user types into the input field.
 
-
-
+console.log(userTypes);
 
 
 // ********************** Exercise 4 ********************** //
 // Change the background color of the <div> element with the ID "container" to blue.
+
+const ccontainerDiv = document.getElementById('container');
+containerDiv.style.backgroundColor = 'blue';
--- website-2/scripts.js
+++ website-2/scripts.js
@@ -2,20 +2,28 @@
 
 function addItem() {
     // Get the user input
-    let newItemText = // add your code here 
+    let newItemText = document.getElementById("newitem").value;
 
     // Check if the input is empty. If it is empty, return a alert that says, "Please enter an item"
     
     //add your code here
 
+    "Please enter an item"
+    if (newItemText.trim() === '') {
+        alert("Please enter an item");
+        return;
+    }
+
     // Create a new list item element
-    var listItem = // add your code here
+    var listItem = document.createElement("li") // add your code here
+
     
     // Set the text content of the new list item
     listItem.textContent = newItemText;
 
     // Get the list where we want to add the new item
-    var itemList = // add your code here
+    var itemList = document.getElementById("itemList");
+    // add your code here
 
     // Append the new list item to the list
     itemList.appendChild(listItem);
@@ -23,3 +31,5 @@ function addItem() {
     // Clear the input field
     document.getElementById("newItem").value = "";
 }
+
+document.getElementById("addButton").addEventListener("click", addItem);
